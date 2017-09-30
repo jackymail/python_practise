6d24ef28dccfae7b2786c52283ebe6ccbf7c34c4
jackmail <peterpainkiller@gmail.com>
2017-09-30 14:59:34 +0800

    提交第四部代码，主要是修改页面样式排版

--- Django/samplesite/polls/admin.py
+++ Django/samplesite/polls/admin.py
@@ -4,6 +4,22 @@ from django.contrib import admin
 
 from.models import Question,Choice
 
-admin.site.register(Question)
+#class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = [
+        (None,{'fields' : ['question_text']}),
+        ('Date Infomation',{'fields' : ['pub_date'],
+         'classes' : ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text','pub_date','was_published_recently')
+
+
+admin.site.register(Question,QuestionAdmin)
 
 admin.site.register(Choice)
--- Django/samplesite/polls/models.py
+++ Django/samplesite/polls/models.py
@@ -17,9 +17,12 @@ class Question(models.Model):
         now = timezone.now()
 
         return now-datetime.timedelta(days=1)  <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = "Published Recently?"
 
-
-
+    list_filter = ['pub_date']
+    search_fields = ['question.text']
 
 
 class Choice(models.Model):
